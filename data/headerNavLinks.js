027c89612fedc9de48c09c57f384a29451f3d065
Justin Cheung <j43cheun@uwaterloo.ca>
2022-09-11 16:49:19 -0700

    Added Resume link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://github.com/j43cheun/Resume/blob/main/resume.pdf', title: 'Resume' },
 ]
 
 export default headerNavLinks
