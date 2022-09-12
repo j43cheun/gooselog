449229aaa7ff2a34f1b6d034859ec3bf256926f5
Justin Cheung <j43cheun@uwaterloo.ca>
2022-09-11 18:50:15 -0700

    Added Projects page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,38 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'SeeFood',
+    description: `A simple iOS app for identifying food. Given an image, the app classifies the
+    subject into one of two buckets: hot dog, not a hot dog. Implemented using the Swift UI
+    Framework and Core ML. Inspired by HBO's Silicon Valley.`,
+    href: 'https://github.com/j43cheun/SeeFood',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Garbage Vision',
+    description: `A simple iOS app for identifying trash. Given an image, the app classifies the
+    subject into one of several buckets: Cardboard, Glass, Metal, Paper, Plastic, and Trash.
+    Implemented using the Swift UI Framework and Core ML.`,
+    href: 'https://github.com/j43cheun/GarbageVision',
+  },
+  {
+    title: 'Nested Resiliency for S2D',
+    description: `A Storage Spaces Direct (S2D) feature for 2-node Failover Clusters to support
+    provisioning, deploying, and managing Clustered Storage Spaces that can survive an additional
+    drive-level failure after a node-level failure.`,
+    href: 'https://docs.microsoft.com/en-us/azure-stack/hci/concepts/nested-resiliency',
+  },
+  {
+    title: 'Fourth Year Design Project',
+    description: `A distributed garbage collection system aimed at solving a theoretical littering
+    problem. It leverages crowd-sourced data to determine where to position garbage bins within a
+    given open area.`,
+    href: 'https://github.com/j43cheun/FYDP',
+  },
+  {
+    title: 'Science Quiz 2007',
+    description: `Science Quiz 2007 is an interactive science quiz program written in Turing. The
+    program uses imported JPEG images and WAV audio files to test users on basic concepts from
+    ecology and chemistry in Grade 10 science.`,
+    href: 'https://github.com/j43cheun/ScienceQuiz2007',
   },
 ]
 
